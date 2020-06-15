f7c4adbb642b53c6bc747ddb485a0291749e2044
Craig Carpenter <cacarpe@gmail.com>
2020-06-14 21:28:01 -0600

    More readme docs, interpolate variables in the URL with environment and make http request

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# gopostal
+# GoPostal
 Go based tool for managing Postman files
+
+Current Version 0.1.0
+
+GoPostal can read and display postman collections and postman environment files
+
+A single console based user-interface is presented
+
+## Roadmap
+* v0.2.0 - make requests, show response
+* v0.3.0 - run attached scripts and update vars
+* v0.5.0 - write postman collection changes
+* v0.6.0 - open and show multiple postman collections
+* v0.7.0 - ???
+* v1.0.0 - profit
--- gp/client.go
+++ gp/client.go
@@ -1,17 +1,64 @@
 package gp
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/cacarpenter/gopostal/postman"
+	"net/http"
+	"regexp"
+	"strings"
 )
 
-func CallRequest(req *postman.Request) error {
-	fmt.Println("call ", req.Url.Raw)
+func CallRequest(pmreq *postman.Request) error {
+	fmt.Println("call", replaceVariables(pmreq.Url.Raw))
 
+	interUrl := replaceVariables(pmreq.Url.Raw)
+	httpClient := http.Client{}
+	httpReq, err := http.NewRequest(pmreq.Method, interUrl, strings.NewReader(pmreq.Body.Raw))
+	if err != nil {
+		return err
+	}
+
+	httpResp, err := httpClient.Do(httpReq)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(httpResp.StatusCode)
 	return nil
 }
 
 func replaceVariables(raw string) string {
+	r, err := regexp.Compile("\\{\\{\\w+\\}\\}")
+	if err != nil {
+		panic(err)
+	}
+
 	sess := CurrentSession()
 
+	var buffer bytes.Buffer
+	fasi := r.FindAllStringIndex(raw, -1)
+	prevIdx := 0
+	for _, matchIndices := range fasi {
+		// TODO should be able to remove this
+		if len(matchIndices) != 2 {
+			panic(fmt.Errorf("Thought length would be 2 but it was %d\n", len(matchIndices)))
+		}
+
+		// chars before this match
+		if matchIndices[0] > prevIdx {
+			buffer.WriteString(raw[prevIdx:matchIndices[0]])
+		}
+		varstr := raw[matchIndices[0]+2 : matchIndices[1]-2]
+		val := sess.Get(varstr)
+		buffer.WriteString(val)
+
+		prevIdx = matchIndices[1]
+	}
+	// some literal text remains
+	if prevIdx < len(raw)-1 {
+		buffer.WriteString(raw[prevIdx:len(raw)])
+	}
+
+	return buffer.String()
 }
